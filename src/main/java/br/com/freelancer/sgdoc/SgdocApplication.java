1da191a50ac74dd809a15a42b5fa03c632054794
aluno <aluno@LAB12-11.expert.local>
2019-08-10 14:43:57 -0300

    correções

--- src/main/java/br/com/freelancer/sgdoc/SgdocApplication.java
+++ src/main/java/br/com/freelancer/sgdoc/SgdocApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan("br.com.freelancer.model")
-@ComponentScan("br.com.freelancer.sgdoc.*")
+@ComponentScan("br.com.freelancer.*")
 public class SgdocApplication {
 
 	public static void main(String[] args) {
